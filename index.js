308d1c116d3b9c7da9355d395613b37ec15c1767
bouquet1 <bukuleta@gmail.com>
2023-04-15 20:25:22 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("The DOM has loaded");
+// });
+
+//  const changeText = document.querySelector('p');
+//  changeText.innerText = "This is really cool!";
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const changeText = document.getElementById('text');
+    changeText.textContent = "This is really cool!";
+});
